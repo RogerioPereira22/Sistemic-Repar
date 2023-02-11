6c09c0eeb3b0e360950a8dca55bc118b375a617a
Rogério Pereira <rogeriojr2116@gmail.com>
2023-02-11 16:24:02 -0300

    init

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import payload from 'payload';
+import nodemailerSendgrid from 'nodemailer-sendgrid';
+
+require('dotenv').config();
+const app = express();
+
+// Redirect root to Admin panel
+app.get('/', (_, res) => {
+  res.redirect('/admin');
+});
+
+const sendGridAPIKey = process.env.SENDGRID_API_KEY;
+
+const sendgridConfig = {
+  transportOptions: nodemailerSendgrid({
+    apiKey: sendGridAPIKey,
+  }),
+};
+
+const start = async () => {
+  // Initialize Payload
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    mongoURL: process.env.MONGODB_URI,
+    express: app,
+    email: {
+      fromName: 'Payload CMS',
+      fromAddress: 'info@payloadcms.org',
+      ...sendgridConfig,
+    },
+    onInit: () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    },
+  })
+
+  // Add your own express routes here
+
+  app.listen(process.env.PORT, async () => {
+    payload.logger.info(`Server listening on port ${process.env.PORT}`);
+  });
+}
+
+start();
