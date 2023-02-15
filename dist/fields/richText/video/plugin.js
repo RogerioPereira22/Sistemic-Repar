bd122647f6eb3ed704fc7c59714d449442292fad
RogerioPereira22 <rogeriojr2116@gmail.com>
2023-02-15 17:43:43 -0300

    update

--- dist/fields/richText/video/plugin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var withVideo = function (incomingEditor) {
-    var editor = incomingEditor;
-    var isVoid = editor.isVoid;
-    editor.isVoid = function (element) { return (element.type === 'video' ? true : isVoid(element)); };
-    return editor;
-};
-exports.default = withVideo;
--- dist/utilities/formatPreviewURL.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatPreviewURL = void 0;
-var formatPagePath_1 = require("./formatPagePath");
-var formatPreviewURL = function (collection, doc) {
-    return "".concat(process.env.PAYLOAD_PUBLIC_APP_URL, "/api/preview?url=").concat((0, formatPagePath_1.formatPagePath)(collection, doc));
-};
-exports.formatPreviewURL = formatPreviewURL;
