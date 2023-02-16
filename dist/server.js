7fa40f0c7a75b92264ca0db9e726db8a29221de0
RogerioPereira22 <rogeriojr2116@gmail.com>
2023-02-15 22:59:49 -0300

    tsc

--- dist/server.js
+++ dist/server.js
@@ -49,8 +49,6 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
-import __esModule from './app';
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var payload_1 = __importDefault(require("payload"));
